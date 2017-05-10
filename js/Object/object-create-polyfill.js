fffd8109f3819db02274c678ffa38a513f09eea8
anning <anning@zbj.com>
2017-05-10 21:48:14 +0800

    add Object.create polyfill

---
+++ js/Object/object-create-polyfill.js
@@ -0,0 +1,25 @@
+/**
+ * Author : Amnhh
+ * Email : amnhhlod@gmail.com
+ * Date : 2017/5/10
+ */
+
+/**
+ * Polyfill of Object.create
+ * @param proto
+ * @private
+ */
+function _create (proto) {
+  var nativeCreate = Object.create;
+  // If nativeCreate is a native function.
+  if (typeof nativeCreate === 'function' && /\[native code\]/.test(nativeCreate.toString())) {
+    return nativeCreate(proto);
+  }
+
+  // Get a constructor.
+  function F () {}
+  // Prototype bind.
+  F.prototype = proto;
+
+  return new F();
+};
