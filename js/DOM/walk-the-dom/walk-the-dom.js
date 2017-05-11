62ee9293f302409ef7c30119a41db28b7e0b47ab
amnhh <2570079254@qq.com>
2017-05-11 11:05:05 +0800

    add walk the dom

---
+++ js/DOM/walk-the-dom/walk-the-dom.js
@@ -0,0 +1,51 @@
+/**
+ * Author : Amnhh
+ * Email : amnhhlod@gmail.com
+ * Date : 2017/5/11
+ */
+
+/**
+ * 蝴蝶书上的例子
+ * @param node
+ * @param func
+ */
+var walkTheDOM = function walk (node, func) {
+  func.call(node);
+
+  node = node.firstChild;
+
+  if (node) {
+    walk(node, func);
+    node = node.nextSibling;
+  }
+};
+
+/**
+ * 自己的想法
+ */
+
+var _walkTheDOM = function walk (node, func) {
+  // 不是 node 的时候抛错
+  // 只在第一次进入的时候检查, 否则不检查 => 也可以手动指定不检查
+  if ( typeof node.nodeType === 'undefined') {
+    throw new TypeError('Need an node');
+  }
+
+  var _slice = [].slice, // 取出来 slice
+
+    childNodesList = _slice.call(node.childNodes), // 子元素list
+
+    len = childNodesList.length; // 子元素 list 的长度
+
+  func.call(node);
+
+  childNodesList.map(val => {
+    // 过滤掉不是节点的元素
+    if (val.nodeType !== 1) {
+      return;
+    }
+
+    walk(val, func);
+  });
+
+}
