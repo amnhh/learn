27a05bff8b368b61606cca25ffc2f3a78f53a975
amnhh <2570079254@qq.com>
2017-06-15 13:19:08 +0800

    [ node ] Add require json demo

---
+++ nodejs/require-json/use-data.js
@@ -0,0 +1,21 @@
+/**
+ * Author : anning
+ * Date : 2017/6/15
+ * Email : amnhhlod@gmail.com
+ * Description : 使用 json
+ */
+
+// import data.json
+// Here we add `.json` to make node's search more faster
+// Because node will add `.js`, `.node` and `.json` to match the file package you required
+var _data = require('./data.json');
+
+// { name: 'Amnhh', job: 'Front-End Developer', age: 23 }
+console.log(_data);
+// object
+// Because node do with package with json type.
+// Will use `fs` system to get the value.
+// Then use `JSON.parse` to make it be a object.
+console.log(typeof _data);
+
+
